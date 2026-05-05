17d8f83bd5d7993d4c6430980665f4e62103ee60
anleylafleur <anleylafleur@gmail.com>
2026-05-05 19:09:13 +1000

    Increase Azure SQL connection timeout

--- db.py
+++ db.py
@@ -1,11 +1,22 @@
+import os
 import pyodbc
 
 def get_connection():
+    server = os.getenv("DB_SERVER")
+    database = os.getenv("DB_NAME")
+    username = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    driver = os.getenv("DB_DRIVER", "ODBC Driver 18 for SQL Server")
+
     conn_str = (
-        "DRIVER={ODBC Driver 18 for SQL Server};"
-        "SERVER=localhost;"
-        "DATABASE=SoccerRegistryDB;"
-        "Trusted_Connection=yes;"
-        "TrustServerCertificate=yes;"
+        f"DRIVER={{{driver}}};"
+        f"SERVER=tcp:{soccerregistry},1433;"
+        f"DATABASE={SoccerRegistryDB};"
+        f"UID={anleylafleur@hotmail.com};"
+        f"PWD={#Champ195472};"
+        "Encrypt=yes;"
+        "TrustServerCertificate=no;"
+        "Connection Timeout=60;"
     )
-    return pyodbc.connect(conn_str)
+
+    return pyodbc.connect(conn_str, timeout=60)
